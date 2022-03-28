4b511f92ecccd43fb33982f5c76dcf3732a77984
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-03-28 14:01:19 +0300

    feat: add reducer login

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,12 +1,50 @@
 import { RootState } from 'types';
+import { Action } from 'actions';
 
-export type Action = {
-  type: string;
+const initialState: RootState = {
+  currentStroke: {
+    points: [],
+    color: '#000',
+  },
+  strokes: [],
+  historyIndex: 0,
 };
 
-export const rootReducer = (
-  state: RootState,
-  action: Action,
-): RootState => {
-  return state;
+export const rootReducer = (state = initialState, action: Action): RootState => {
+  switch (action.type) {
+    case 'BEGIN_STROKE': 
+      return {
+        ...state,
+        currentStroke: {
+          ...state.currentStroke,
+          points: [action.payload.point],
+        },
+      };
+    case 'UPDATE_STROKE': 
+      return {
+        ...state,
+        currentStroke: {
+          ...state.currentStroke,
+          points: [...state.currentStroke.points, action.payload.point],
+        },
+      };
+    case 'END_STROKE': 
+      if (!state.currentStroke.points.length) {
+        return state;
+      }
+
+      return {
+        ...state,
+        currentStroke: {
+          ...state.currentStroke,
+          points: [],
+        },
+        strokes: [
+          ...state.strokes,
+          state.currentStroke,
+        ],
+      };
+    default:
+      return state;
+  }
 };
