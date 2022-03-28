adad1c6e085dfa07543092293c969cae8db41110
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-03-28 13:37:18 +0300

    feat: add canvas events blank handlers

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import { FC, useRef } from 'react';
 const App: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const startDrawing = () => {};
+
+  const endDrawing = () => {};
+
+  const draw = () => {};
+
   return (
     <div className="app">
-      <canvas ref={canvasRef} />
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseOut={endDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
     </div>
   );
 };
